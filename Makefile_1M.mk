328f0e1f5041b40f853a206dcb5c0381d9e3f65b
Christopher Haster <geky@geky.net>
2025-05-19 01:09:56 -0500

    Added Makefile_1M.mk - for 1MiB results
    Currently limited to bench_vs_lfs2_fwrite and bench_vs_lfs2_logging, but
    this is useful and a bit more flexible than adding even more make rules.
    
    Functionality is limited (other benches may break), but at least
    bench/plot/all default rules are overridden to do what you would expect:
    
      $ make -fMakefile_1M.mk bench -j && make -fMakefile_1M.mk plot

---
+++ Makefile_1M.mk
@@ -0,0 +1,35 @@
+# overrideable build dir, default to ./build_1M
+export BUILDDIR ?= build_1M
+# overrideable results dir, default to ./results_1M
+export RESULTSDIR ?= results_1M
+# overrideable plots dir, defaults ./plots_1M
+export PLOTSDIR ?= plots_1M
+
+
+# increase bench sizes to 1 MiB
+export BENCHFLAGS += -DDISK_SIZE=67108864
+export BENCHFLAGS += -DSIZE=1048576
+export BENCHFLAGS += -DSTEP=64
+
+
+# override the default bench/plot rules
+
+## Run all benchmarks!
+.PHONY: bench bench-all
+bench bench-all: \
+		bench-vs-lfs2-fwrite \
+		bench-vs-lfs2-logging
+
+## Plot all benchmarks!
+.PHONY: all plot plot-all
+all plot plot-all: \
+		plot-vs-lfs2-fwrite \
+		plot-vs-lfs2-logging
+
+
+# we just set the above defines, the default Makefile does all the
+# real work
+FORCE:;
+%: FORCE
+	@$(MAKE) $@
+
