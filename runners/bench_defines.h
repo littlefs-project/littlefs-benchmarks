390f3777f10537987bf6a71cd55c70b370cad85f
Christopher Haster <geky@geky.net>
2026-08-10 14:04:27 -0500

    Adopted upstream lookahead/gbmap thresh changes
    -1 now disables, which is _not_ what we want by default.

--- runners/bench_defines.h
+++ runners/bench_defines.h
@@ -153,7 +153,7 @@
                                                 0)                          )
     BENCH_DEFINE(GC_FLAGS,              LFS3_GC_GC                          )
     BENCH_DEFINE(GC_STEPS,              0                                   )
-    BENCH_DEFINE(GC_LOOKAHEAD_THRESH,   -1                                  )
+    BENCH_DEFINE(GC_LOOKAHEAD_THRESH,   BLOCK_COUNT                         )
     BENCH_DEFINE(GC_LOOKGBMAP_THRESH,   BLOCK_COUNT - BLOCK_COUNT/4         )
     // NOTE this matches spiffs, TODO what about yaffs2?
     BENCH_DEFINE(GC_PREERASE_COUNT,     3                                   )
